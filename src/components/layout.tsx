5f39b917fa3c44b2b4536a232a1e9690d2eca9d6
Jun1 <superbigtuna@gmail.com>
2024-06-11 15:31:28 +0900

    2

---
+++ src/components/layout.tsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout(){
+    return <>
+        <h2>Layout</h2>
+        <Outlet/>
+    </>
+}
---
+++ src/routes/home.tsx
@@ -0,0 +1,4 @@
+export default function home(){
+    return <h1>Home!</h1>
+
+}
